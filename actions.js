f07646e955c034ff4ba6f0eae7815512d7799360
jquique7 <jquique7@hotmail.com>
2013-11-23 17:11:19 -0600

    geoapp

--- actions.js
+++ actions.js
@@ -1,28 +1,107 @@
-// acciones
-$(function(){
-	  document.addEventListener("deviceready",function(){	
-	   navigator.geolocation.getCurrentPosition(function(pos){ //funcion pos
-	     initialize(pos.coords.latitude,pos.coords.longitude);  
-	   }, function(err){
-		  alert('Error: '+err.code); 
-	   });
-	  }, false);
-});
-
-
-function initialize(lat, lon) { //lat, lon: parametros de latitud y longitud
-				//Posición del mapa
-				var latlng = new google.maps.LatLng(lat, lon);//latitud,longitud, a este obj le pasamos las coordenadas
-				var myOptions = {
-					zoom: 16,   //zoom
-					center: latlng,  //centro: latitud y longitud
-					mapTypeId: google.maps.MapTypeId.ROADMAP   // el mapa de google para que se vea 
-				};
-				var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);//obj tipo mapa, crea el canvas
-				//Marcador
-				var marker = new google.maps.Marker({ //obj tipo marcador, atributo position, map y tittle "mi posicion
-					position: latlng, 
-					map: map, 
-					title:"Mi posición" //tittle "mi posicion"
-				});
-			}
+var rango = {latitud:100,longitud:100}; 
+ 
+ function initialize() {
+	      document.addEventListener("deviceready", function(){
+			  crearTabla();
+			  obtenerRegistros();
+			  var lat = geoloc().lat;
+			  var lng = geoloc().lng;
+		  var latlng = new google.maps.LatLng(lat, lng);
+        var mapOptions = {
+          center: latlng,
+          zoom: 12,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+		var marker = new google.maps.Marker({
+      position:  latlng,
+      map: map,
+      title:"Hello World!"
+    }); 
+	localizador(lat,lng);
+   },false);
+  }
+  
+  function geoloc(){
+	  var obj = {lat:null,lng:null};
+	 navigator.geolocation.getCurrentPosition(function(p){
+		 lat = p.coords.latitude;
+		 lng = p.coords.longitude;
+	 },function(err){
+	    alert(err.code);
+	 });  
+  return obj;
+  }
+ 
+ function accesoDB(){
+	var db =  window.openDatabase("lugares","0.1","Lugares",200000);
+	return db;
+ }
+ 
+ function obtenerRegistros(){
+  $.ajax({
+     type: "POST",
+     url: "http://10.175.11.51/lugares.php",
+     data: "obtener lugares"
+}).done(function(msg) {
+	borrarRegistros();
+    msg = JSON.parse(msg);
+	for(i=0;i<msg.length;i++){
+	 guardarLugares(msg[i].latitud, msg[i].longitud);
+	}
+  }); 
+ }
+ 
+ function guardarLugares(lat, lng){
+	 accesoDB().transaction(function(tx){
+		 tx.executeSql('INSERT INTO lugares (latitud,longitud) VALUES("'+lat+'","'+lng+'")');
+	 },function(err){
+		 alert(err.code);
+	 },function(){
+		 alert('guardado');
+ });
+ }
+ function crearTabla(){
+	accesoBD().transaction(function(tx){
+	  tx.executeSql('CREATE TABLE IF NOT EXIST lugares(id unique,latitud,longitud)');
+	},function(err){
+		alert(err.code);
+	},function(){
+	    var x = null;	
+	}); 
+ }
+ 
+ function borrarRegistros(){
+	 accesoBD().transaction(function(tx){
+		  tx.executeSql('DELETE FROM lugares');
+	},function(err){
+		alert(err.code);
+	},function(){
+	    var x = null;	
+	}); 
+ }
+ 
+ function localizador(lat,lng){
+	 var marcadores = [];
+	 accesoBD().transaction(function(tx){
+		  tx.executeSql('SELECT * FROM lugares',[],function(tx2,res){
+			  for(i=0;i<res.rows.length;i++){
+				  var latitud = res.rows.item(i).latitud;
+				  var longitud = res.rows.item(i).longitud;
+				  if(latitud <= rango.latitud && latitud >= -rango.latitud && longitud <= rango.longitud && longitud >= -rango.longitud){
+				  marcadores[i] = new google.maps.Marker({position: new google.maps.LatLng(res.rows.item(i).latitud,res.rows.item(i).longitud),
+				  map: map,
+				  title: "Marcador " + i
+				  });
+				  }
+			  }
+		  },function(err){
+			  alert(err.code);
+		  });
+	},function(err){
+		alert(err.code);
+	},function(){
+	    alert("Localizado");
+	}); 
+ }
+ 
